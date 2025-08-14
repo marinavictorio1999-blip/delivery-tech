abc0fd76a02a238c27040ba46add187a0b04dc70
Marina Victório <marinavictorio1999@hotmail.com>
2025-08-13 22:49:47 -0300

    Salvar

---
+++ src/main/java/com/deliverytech/delivery_api/model/Cliente.java
@@ -0,0 +1,36 @@
+package com.deliverytech.delivery_api.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Builder.Default
+    private Boolean ativo = true;
+}
