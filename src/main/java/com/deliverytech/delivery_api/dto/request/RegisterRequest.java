f2abfbfda512b84ec0192d300940c12e7feee33b
Marina Victório <marinavictorio1999@hotmail.com>
2025-08-26 22:21:06 -0300

    Criação de novas pastas

---
+++ src/main/java/com/deliverytech/delivery_api/dto/request/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.deliverytech.delivery_api.dto.request;
+
+import com.deliverytech.delivery_api.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @Email(message = "Email deve ter formato válido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    @NotBlank(message = "Papel é obrigatório")
+    private Role role;
+
+    
+}
